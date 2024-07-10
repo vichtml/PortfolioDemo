a5b18d41653519a20ad2f107816fbda324f7fe77
Alexsander Victor <148028152+vichtml@users.noreply.github.com>
2024-07-10 17:51:10 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,13 @@
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('abrir-menu'); // Toggle para adicionar ou remover a classe
+    overlay.style.display = 'block'; // Mostra o overlay ao abrir o menu
+});
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu'); // Fecha o menu ao clicar no overlay
+    overlay.style.display = 'none'; // Esconde o overlay ao fechar o menu
+});
