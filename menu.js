ef7262b8a46d2afe3a01604092ecf30829ed4f86
Alexsander Victor <148028152+vichtml@users.noreply.github.com>
2024-08-05 17:09:36 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,32 @@
+// menu.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.menu-mobile-button');
+    const menuNav = document.querySelector('.menu-mobile-nav');
+
+    menuButton.addEventListener('click', function() {
+        menuNav.classList.toggle('show');
+    });
+});
+
+// Ajusta o menu conforme o tamanho da tela
+function ajustarMenu() {
+    var menu = document.querySelector('.menu-desktop');
+    if (window.innerWidth <= 768) {
+        menu.style.display = 'none'; // Oculta o menu em telas pequenas
+    } else {
+        menu.style.display = 'flex'; // Exibe o menu em telas grandes
+    }
+}
+
+// Chama a função ao carregar a página e ao redimensionar a janela
+window.addEventListener('load', ajustarMenu);
+window.addEventListener('resize', ajustarMenu);
+
+// Funções para abrir e fechar a navegação lateral
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
