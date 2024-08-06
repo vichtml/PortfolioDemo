c083370ce159004c071b7be13fe9ddaf6f0de836
Alexsander Victor <148028152+vichtml@users.noreply.github.com>
2024-08-06 00:11:47 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,18 @@
-// menu.js
 document.addEventListener('DOMContentLoaded', function() {
     const menuButton = document.querySelector('.menu-mobile-button');
     const menuNav = document.querySelector('.menu-mobile-nav');
 
     menuButton.addEventListener('click', function() {
         menuNav.classList.toggle('show');
+    });
+
+    // Adiciona event listener aos links do menu para fechar o menu ao clicar
+    const menuLinks = document.querySelectorAll('.menu-mobile-nav a');
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            menuNav.classList.remove('show');
+            closeNav();
+        });
     });
 });
 
